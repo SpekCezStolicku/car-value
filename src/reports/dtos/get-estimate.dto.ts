f30375b7cf658580c461e760388f6ef971e50fcb
Speky <martin.mozucha@gmail.com>
2024-10-28 21:23:49 +0100

    build query for estimate report

---
+++ src/reports/dtos/get-estimate.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+  IsLongitude,
+  IsLatitude,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class GetEstimateDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @Transform(({ value }) => parseInt(value))
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @Transform(({ value }) => parseFloat(value))
+  @IsLongitude()
+  lng: number;
+
+  @Transform(({ value }) => parseFloat(value))
+  @IsLatitude()
+  lat: number;
+}
